a86aada189aa39c32b34c33500398c08f85e6364
YueqiCao <756347162@qq.com>
2021-06-05 10:39:33 +0800

    add readme and revise main functions

--- DI distance and similarity/DI_dissimilarity.py
+++ DI distance and similarity/DI_dissimilarity.py
@@ -9,16 +9,51 @@ import numpy as np
 import gudhi as gd
 import matplotlib.pyplot as plt
 
+
+# main function
+def myDissimilarity(X, Y, num_partitions):
+    
+    perX = max(X[:,1]-X[:,0])
+    indX = np.argmax(X[:,1]-X[:,0])
+    perY = max(Y[:,1]-Y[:,0])
+    indY = np.argmax(Y[:,1]-Y[:,0])
+        
+    ## character point
+    chaX = X[indX,:]
+    chaY = Y[indY,:]
+    
+    ## define the searching interval
+    oriDist = gd.bottleneck_distance(X,Y)
+    print("original bottleneck distance", oriDist)
+    upBound = min((perY+2*oriDist)/perX, (max(Y[:,1])+oriDist)/chaX[1])
+    lowBound = max((perY-2*oriDist)/perX, (chaY[1]-oriDist)/max(X[:,1]))
+    
+    ## define partitions
+    dilations = np.linspace(lowBound, upBound, num_partitions)
+    dilaInv = np.append(dilations, 1)
+    
+    bottleneckDistance = []
+    
+    ## compute bottleneck distance
+    for scale in dilaInv:
+        dilaX = scale*X
+        dist = gd.bottleneck_distance(dilaX, Y)
+        bottleneckDistance.append(dist)
+        
+    minBottleneckDistance = min(bottleneckDistance)
+    print("dilation-invariant dissimilarity (left-to-right) is", minBottleneckDistance)
+    
+    return dilaInv, bottleneckDistance, minBottleneckDistance
+
+
+# load data
+    
 #file_list = ['activity_net_euclidean_cosine.npz','activity_net_euclidean.npz']
 #file_list = ['activity_net_euclidean_expmap_poincare.npz','activity_net_poincare.npz']
 file_list = ['mammals_euclidean_cosine.npz','mammals_euclidean.npz']
 #file_list = ['mammals_euclidean_expmap_poincare.npz','mammals_poincare.npz']
 
-### hyperparameters
-N_partitions = 100
-
-### Read files
-dist_list =[np.load(u)['arr_0'] for u in file_list]
+dist_list = [np.load(u)['arr_0'] for u in file_list]
 
 X = dist_list[0][0]
 Y = dist_list[1][0]
@@ -26,45 +61,14 @@ Y = dist_list[1][0]
 X = np.delete(X, -1, axis = 1)
 Y = np.delete(Y, -1, axis = 1)
 
-perX = max(X[:,1]-X[:,0])
-indX = np.argmax(X[:,1]-X[:,0])
-perY = max(Y[:,1]-Y[:,0])
-indY = np.argmax(Y[:,1]-Y[:,0])
+dilaInv, bottleneckDistance, minBottleneckDistance = myDissimilarity(X, Y, 100)
 
-
-### character point
-
-chaX = X[indX,:]
-chaY = Y[indY,:]
-
-### define the searching interval
-oriDist = gd.bottleneck_distance(X,Y)
-print("original bottleneck distance", oriDist)
-upBound = min((perY+2*oriDist)/perX, (max(Y[:,1])+oriDist)/chaX[1])
-lowBound = max((perY-2*oriDist)/perX, (chaY[1]-oriDist)/max(X[:,1]))
-
-### define partitions
-dilations = np.linspace(lowBound, upBound, N_partitions)
-dilaInv = np.append(dilations, 1)
-
-bottleneckDistance = []
-
-#compute bottleneck distance
-for scale in dilaInv:
-    dilaX = scale*X
-    dist = gd.bottleneck_distance(dilaX, Y)
-    bottleneckDistance.append(dist)
-    
-### plot
-minBottleneckDistance = min(bottleneckDistance)
-print('dilation-invariant distance is ', minBottleneckDistance)
+plt.figure()
 label = ['different dilations','no dilation','optimal dilation']
 plt.xlabel("dilation parameters")
 plt.ylabel("bottleneck distances")
-plt.plot(dilaInv[0:len(dilaInv)-1],bottleneckDistance[0:len(bottleneckDistance)-1])
+plt.plot(dilaInv[0:len(dilaInv)-1], bottleneckDistance[0:len(bottleneckDistance)-1])
 # the last element corresponds to no dilation
-plt.axhline(y=bottleneckDistance[-1],color='r',linestyle='-.')
-plt.axhline(y=minBottleneckDistance,color='g',linestyle='-')
-plt.legend(label,loc='best')
-
-
+plt.axhline(y=bottleneckDistance[-1], color='r', linestyle='-.')
+plt.axhline(y=minBottleneckDistance, color='g', linestyle='-')
+plt.legend(label, loc='best')
--- DI distance and similarity/DI_distance.py
+++ DI distance and similarity/DI_distance.py
@@ -9,17 +9,48 @@ import numpy as np
 import gudhi as gd
 import matplotlib.pyplot as plt
 
+# main function
+
+def myDistance(X, Y, num_partitions):
+    
+    ## character point
+    indX = np.argmax(X[:,1]-X[:,0])
+    indY = np.argmax(Y[:,1]-Y[:,0])
+    chaX = X[indX,:]
+    chaY = Y[indY,:]
+    
+    ## define the searching interval
+    B = 1e3 ## translation parameter. This is a bug in gudhi that they donot accept negative inputs
+    oriDist = gd.bottleneck_distance(X+B,Y+B)
+    print("original bottleneck distance", oriDist)
+    upBound = max(Y[:,1])-chaX[1]+oriDist
+    lowBound = chaY[1]-max(X[:,1])-oriDist
+    
+    ## define shifts
+    shifts = np.linspace(lowBound, upBound, num_partitions)
+    shiftInv = np.append(shifts, 0)
+    bottleneckDistance = []
+    
+    ## compute bottleneck distance
+    for scale in shiftInv:
+        shiftX = scale+X
+        dist = gd.bottleneck_distance(shiftX+B, Y+B)
+        bottleneckDistance.append(dist)
+
+    minBottleneckDistance = min(bottleneckDistance)
+    print("dilation-invariant bottleneck distance (using log) is", minBottleneckDistance)
+    
+    return shiftInv, bottleneckDistance, minBottleneckDistance
+
+
+# load data
 
 #file_list = ['activity_net_euclidean_cosine.npz','activity_net_euclidean.npz']
 #file_list = ['activity_net_euclidean_expmap_poincare.npz','activity_net_poincare.npz']
 file_list = ['mammals_euclidean_cosine.npz','mammals_euclidean.npz']
 #file_list = ['mammals_euclidean_expmap_poincare.npz','mammals_poincare.npz']
 
-### hyperparameters
-N_partitions = 100
-
-### Read files
-dist_list =[np.load(u)['arr_0'] for u in file_list]
+dist_list = [np.load(u)['arr_0'] for u in file_list]
 
 X = dist_list[0][0]
 Y = dist_list[1][0]
@@ -27,48 +58,19 @@ Y = dist_list[1][0]
 X = np.delete(X, -1, axis = 1)
 Y = np.delete(Y, -1, axis = 1)
 
-### avoid -inf in log map
-ep=1e-10
+## avoid -inf in log map
+ep = 1e-10
 X = np.log(X+ep)
 Y = np.log(Y+ep)
 
-perX = max(X[:,1]-X[:,0])
-indX = np.argmax(X[:,1]-X[:,0])
-perY = max(Y[:,1]-Y[:,0])
-indY = np.argmax(Y[:,1]-Y[:,0])
+shiftInv, bottleneckDistance, minBottleneckDistance = myDistance(X, Y, 100)
 
-
-### character point
-
-chaX = X[indX,:]
-chaY = Y[indY,:]
-
-### define the searching interval
-B = 1e3 # translation parameter. This is a bug in gudhi that they donot accept negative inputs
-oriDist = gd.bottleneck_distance(X+B,Y+B)
-print("original bottleneck distance", oriDist)
-upBound = max(Y[:,1])-chaX[1]+oriDist
-lowBound = chaY[1]-max(X[:,1])-oriDist
-
-### define shifts
-shifts = np.linspace(lowBound, upBound, N_partitions)
-shiftInv = np.append(shifts, 0)
-
-bottleneckDistance = []
-#compute bottleneck distance
-for scale in shiftInv:
-    shiftX = scale+X
-    dist = gd.bottleneck_distance(shiftX+B, Y+B)
-    bottleneckDistance.append(dist)
-    
-### plot
-minBottleneckDistance = min(bottleneckDistance)
-print('shift-invariant distance is ', minBottleneckDistance)
+plt.figure()
 label = ['different shifts','no shift','optimal shift']
 plt.xlabel("log shift parameters")
 plt.ylabel("bottleneck distances")
-plt.plot(shiftInv[0:len(shiftInv)-1],bottleneckDistance[0:len(bottleneckDistance)-1])
+plt.plot(shiftInv[0:len(shiftInv)-1], bottleneckDistance[0:len(bottleneckDistance)-1])
 # the last element corresponds to no dilation
-plt.axhline(y=bottleneckDistance[-1],color='r',linestyle='-.')
-plt.axhline(y=minBottleneckDistance,color='g',linestyle='-')
-plt.legend(label,loc='best')
+plt.axhline(y=bottleneckDistance[-1], color='r', linestyle='-.')
+plt.axhline(y=minBottleneckDistance, color='g', linestyle='-')
+plt.legend(label, loc='best')
